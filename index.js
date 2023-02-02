2c017755d1d97eff4b9144308c8f1d05e73a5c5c
Salvador Aceves <salvador@aceves.mx>
2023-02-02 16:06:16 -0600

    working revision

---
+++ index.js
@@ -0,0 +1,104 @@
+const R = require('ramda');
+
+const { Client } = require('@elastic/elasticsearch');
+const { env: { elasticLogs, elasticLogIndex = 'apilog_ti2' } } = process;
+
+class Plugin {
+  constructor(params = {}) { // we get the env variables from here
+    this.onRequestStart = this.onRequestStart.bind(this);
+    this.onRequestEnd = this.onRequestEnd.bind(this);
+    Object.entries(params).forEach(([attr, value]) => {
+      this[attr] = value;
+    });
+    return (async () => {
+      if (!elasticLogs) return this;
+      const elasticLogsClient = new Client({ node: elasticLogs });
+
+      const mappings = {
+        dynamic: true,
+        properties: {
+          body: { type: 'object' },
+          client: { type: 'keyword' },
+          date: { type: 'long' },
+          method: { type: 'keyword' },
+          operationId: { type: 'keyword' },
+          params: { type: 'object' },
+          query: { type: 'object' },
+          url: { type: 'text' },
+          responseStatusCode: { type: 'long' },
+          responseTimeInMs: { type: 'long' },
+        },
+      };
+      // setup the index
+      const { body } = await elasticLogsClient.indices.existsTemplate({
+        name: 'apilog',
+      });
+      if (!body) {
+        // the template has to be created
+        console.log('=- creating index template -=');
+        await elasticLogsClient.indices.putTemplate({
+          name: 'apilog',
+          body: {
+            index_patterns: [`${elasticLogIndex}*`],
+            settings: {
+              number_of_shards: 1,
+            },
+            mappings,
+          },
+        });
+        console.log('index template created');
+      } else {
+        // index exists, we might have to updated it
+        const current = R.path(
+          ['body', 'apilog', 'mappings'],
+          await elasticLogsClient.indices.getTemplate({
+            name: 'apilog',
+          }),
+        );
+        if (!R.equals(current, mappings)) {
+          console.log('=- updating index template -=');
+          await elasticLogsClient.indices.putTemplate({
+            name: 'apilog',
+            body: {
+              index_patterns: [`${elasticLogIndex}*`],
+              settings: {
+                number_of_shards: 1,
+              },
+              mappings,
+            },
+          });
+          console.log('updated');
+        }
+      }
+      const exists = await elasticLogsClient.indices.exists({
+        index: elasticLogIndex,
+      });
+      if (!exists || !exists.body) {
+        console.log('=- creating index =-');
+        await elasticLogsClient.indices.create({
+          index: elasticLogIndex,
+        });
+        console.log('index created');
+      }
+      return this;
+    })();
+  }
+
+  async onRequestStart(requestBody) {
+    await elasticLogsClient.index({
+      index: elasticLogIndex,
+      id: requestBody.requestId,
+      body: requestBody,
+    });
+  }
+
+  async onRequestEnd(body) {
+    await elasticLogsClient.index({
+      index: elasticLogIndex,
+      id: body.requestId,
+      body,
+    });
+  }
+}
+
+module.exports = Plugin;
